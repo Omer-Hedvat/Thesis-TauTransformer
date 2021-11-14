03eb170c749512c03b2532df346be98d3007fc68
ChenBenMayor <chen.benm@gmail.com>
2021-11-14 18:15:25 +0200

    heatmaps fixed

--- main.py
+++ main.py
@@ -82,24 +82,40 @@ def main():
         std = var ** 0.5
         return mean, std
 
-    def norm_by_dist_type(df, feature, dist_type):
-        assert dist_type in ('wasserstein_dist', 'bhattacharyya_dist', 'jensen_shannon_dist', 'hellinger_dist', 'jm_dist')
-        _, dist_dict = calc_dist(dist_type, df, 'label')
-        feature_mat = dist_dict[feature]
+    def norm_by_dist_type(feature_mat):
         mean, std = calc_mean_std(feature_mat)
         norm_feature_mat = (feature_mat - mean) / std
         return norm_feature_mat
 
-    def export_plots(df, feature, dist_type1, dist_type2):
-        print(f'{feature=}')
-        norm_feature_mat1 = norm_by_dist_type(df, feature, dist_type1)
-        norm_feature_mat2 = norm_by_dist_type(df, feature, dist_type2)
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-        fig.set_figheight(6)
-        fig.set_figwidth(12)
+    def export_heatmaps(df, features, dist_type1, dist_type2, to_norm):
+        assert dist_type1 in (
+        'wasserstein_dist', 'bhattacharyya_dist', 'jensen_shannon_dist', 'hellinger_dist', 'jm_dist')
+        assert dist_type2 in (
+        'wasserstein_dist', 'bhattacharyya_dist', 'jensen_shannon_dist', 'hellinger_dist', 'jm_dist')
+        _, dist_dict1 = calc_dist(dist_type1, df, 'label')
+        _, dist_dict2 = calc_dist(dist_type2, df, 'label')
 
-        sns.heatmap(norm_feature_mat1, annot=True, linewidths=.5, ax=ax1).set(title=dist_type1)
-        sns.heatmap(norm_feature_mat2, annot=True, linewidths=.5, ax=ax2).set(title=dist_type2)
+        cols = [dist_type1, dist_type2]
+        rows = ['feature {}'.format(row) for row in features]
+        fig, axes = plt.subplots(nrows=len(features), ncols=2, figsize=(8, 25))
+
+        for i, feature in zip(range(len(rows)), features):
+            feature_mat1 = dist_dict1[feature]
+            feature_mat2 = dist_dict2[feature]
+            if to_norm:
+                feature_dist_mat1 = norm_by_dist_type(feature_mat1)
+                feature_dist_mat2 = norm_by_dist_type(feature_mat2)
+            else:
+                feature_dist_mat1 = feature_mat1
+                feature_dist_mat2 = feature_mat2
+            sns.heatmap(feature_dist_mat1, annot=True, linewidths=.5, ax=axes[i, 0])
+            sns.heatmap(feature_dist_mat2, annot=True, linewidths=.5, ax=axes[i, 1])
+
+        for axc, col in zip(axes[0], cols):
+            axc.set_title(col)
+
+        for axr, row in zip(axes[:, 0], rows):
+            axr.set_ylabel(row, rotation=90, size='large')
 
         plt.show()
 
