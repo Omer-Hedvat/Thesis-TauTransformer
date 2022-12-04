2ebb9ba903716b5b10abaebc3a94af98c8973689
omerh <omer.hedvat@gmail.com>
2022-12-04 13:32:27 +0200

    1. tausformer_main.py addition 2. types into tausformer __init__() 3. minor fix

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def main():
         'features_percentage': [0.02, 0.05, 0.1, 0.2, 0.3, 0.5],
         'dist_functions': ['wasserstein', 'jm', 'hellinger'],
         'nrows': 10000,
-        'features_to_reduce_prc': [0.0, 0.2, 0.35, 0.5],
+        'features_to_eliminate_prc': [0.0, 0.2, 0.35, 0.5],
         'dm_dim': [2],
         'alpha': 1,
         'eps_type': 'maxmin',
