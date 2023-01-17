84fd987023140e1ce13b20df73ef9f2c011af2c0
omerh <omer.hedvat@gmail.com>
2023-01-17 09:10:43 +0200

    grid search completed

--- grid_search.py
+++ grid_search.py
@@ -8,19 +8,15 @@ from utils.machine_learning import predict
 from utils.timer import Timer
 
 
-def grid_serach(config):
-
+def grid_search(config):
+    dataset = config['dataset_name']
     features_percentages = [0.02, 0.05, 0.1, 0.2, 0.3, 0.5]
-
 
     dm_params = {
         'dim': 2,
         'alpha': 1,
         'eps_type': 'maxmin'
     }
-
-    for feature_prc in features_percentages:
-        config['feature_percentage'] = feature_prc
 
     grid_search_dict = {
         'features_to_eliminate_prc': [0.0, 0.2, 0.35, 0.5],
@@ -43,9 +39,11 @@ def grid_serach(config):
         k = percentage_calculator(feature_prc, array=all_features)
         if k < 1 or k == len(all_features):
             continue
+
         for idx, permutation in enumerate(permutations_list_of_dicts):
             print(f"{idx=}, {permutation=}")
             dm_params['epsilon_factor'] = permutation['epsilon_factor']
+
             for kfold_iter in range(1, config['kfolds'] + 1):
                 train_set, val_set = kfolds_split(data_norm, kfold_iter, n_splits=config['kfolds'], random_state=0)
                 X_train, y_train, X_test, y_test = arrange_data_features(train_set, val_set, all_features)
@@ -58,6 +56,7 @@ def grid_serach(config):
 
                 kmeans_acc_agg.append(kmeans_acc)
                 timer_list.append(timer)
+
             acc_result = round(lists_avg(kmeans_acc_agg), 6)
             timer_avg = round(lists_avg([t.to_int() for t in timer_list]), 6)
             results_list.append([feature_prc, *list(permutation.values()), acc_result, timer_avg])
@@ -80,9 +79,10 @@ def main():
     ]
 
     for dataset, label in datasets:
+        print(f"{dataset=}")
         config['dataset_name'] = dataset
         config['label_column'] = label
-        grid_serach(config)
+        grid_search(config)
 
 
 if __name__ == '__main__':
